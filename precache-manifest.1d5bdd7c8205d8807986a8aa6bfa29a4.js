034d0e8f90068883d670c93b018591710990aab3
Pierre Gerungan <pierreasd@gmail.com>
2021-02-04 16:21:52 +0700

    Updates

---
+++ precache-manifest.1d5bdd7c8205d8807986a8aa6bfa29a4.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c04626d02c57887712e3295ed86f2958",
+    "url": "/frontend/index.html"
+  },
+  {
+    "revision": "8a0d7597586d4c66c886",
+    "url": "/frontend/static/css/main.50c859be.chunk.css"
+  },
+  {
+    "revision": "60f2e771a30cecec2208",
+    "url": "/frontend/static/js/2.4e791ac0.chunk.js"
+  },
+  {
+    "revision": "8dbc9bd92b344a5deca62eeacc09fc2d",
+    "url": "/frontend/static/js/2.4e791ac0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a0d7597586d4c66c886",
+    "url": "/frontend/static/js/main.80481164.chunk.js"
+  },
+  {
+    "revision": "a03876f053cc39d2eded",
+    "url": "/frontend/static/js/runtime-main.76e94875.js"
+  },
+  {
+    "revision": "f1d71f777331fd7e3de116edf4ee3b67",
+    "url": "/frontend/static/media/avatar.f1d71f77.jpg"
+  },
+  {
+    "revision": "e52419719dd0c4c8b172136f0b625008",
+    "url": "/frontend/static/media/bg.e5241971.jpg"
+  },
+  {
+    "revision": "b08234a2b7e8e4fb2e3c4e713252021c",
+    "url": "/frontend/static/media/bg7.b08234a2.jpg"
+  },
+  {
+    "revision": "56633ed3f62f39d71f571374a6409e65",
+    "url": "/frontend/static/media/christian.56633ed3.jpg"
+  },
+  {
+    "revision": "c058841b1dd64e5f79c6348b24cfb78d",
+    "url": "/frontend/static/media/clem-onojegaw.c058841b.jpg"
+  },
+  {
+    "revision": "216ef03c54bc13771c5e1b8d8f8d5926",
+    "url": "/frontend/static/media/clem-onojeghuo.216ef03c.jpg"
+  },
+  {
+    "revision": "9813593cc577a319a2201342ef0fb237",
+    "url": "/frontend/static/media/cynthia-del-rio.9813593c.jpg"
+  },
+  {
+    "revision": "59ab7a339d3c2b757e28d471eb44d518",
+    "url": "/frontend/static/media/g1.59ab7a33.JPG"
+  },
+  {
+    "revision": "33502fc145fe5f1ee25c3b861056bfc8",
+    "url": "/frontend/static/media/g2.33502fc1.JPG"
+  },
+  {
+    "revision": "2b3e7528e86870556cdca4f475da6ccc",
+    "url": "/frontend/static/media/g3.2b3e7528.JPG"
+  },
+  {
+    "revision": "e7a64174046f0daa6579e8d29522f485",
+    "url": "/frontend/static/media/g4.e7a64174.JPG"
+  },
+  {
+    "revision": "aca6841a5bc245059c557cc229d435c6",
+    "url": "/frontend/static/media/g5.aca6841a.JPG"
+  },
+  {
+    "revision": "becba142d3cf76820e308564ff4d8c44",
+    "url": "/frontend/static/media/g6.becba142.JPG"
+  },
+  {
+    "revision": "6317a64397fc0ce3068edc8fd9bb0289",
+    "url": "/frontend/static/media/gold.6317a643.png"
+  },
+  {
+    "revision": "f4555484e20fed7c41699920c63750ec",
+    "url": "/frontend/static/media/gos-mill.f4555484.jpeg"
+  },
+  {
+    "revision": "e0508cc923eb0b5e68ca6783c53d0f1d",
+    "url": "/frontend/static/media/kendall.e0508cc9.jpg"
+  },
+  {
+    "revision": "6ba0b13e224c1688c00e1e60a69a4a55",
+    "url": "/frontend/static/media/logo.6ba0b13e.png"
+  },
+  {
+    "revision": "4575c40bfb8dec6713d2da51c4c4066e",
+    "url": "/frontend/static/media/mariya-georgieva.4575c40b.jpg"
+  },
+  {
+    "revision": "4112cbc1477d9e149033c5df66087e16",
+    "url": "/frontend/static/media/olu-eletu.4112cbc1.jpg"
+  },
+  {
+    "revision": "4ad9570e79f69480375fc44947c65675",
+    "url": "/frontend/static/media/opa.4ad9570e.jpg"
+  },
+  {
+    "revision": "baf6b40a654b078399e93e3d9cb6d455",
+    "url": "/frontend/static/media/profile-bg.baf6b40a.jpg"
+  },
+  {
+    "revision": "cf8b686b294041d0925f4e745b1fabb9",
+    "url": "/frontend/static/media/sign.cf8b686b.jpg"
+  },
+  {
+    "revision": "ae0150c08dbcc95e4f50458e02e5bd5c",
+    "url": "/frontend/static/media/studio-1.ae0150c0.jpg"
+  },
+  {
+    "revision": "76e2987ed95634136dd22d4d9e1009a7",
+    "url": "/frontend/static/media/studio-2.76e2987e.jpg"
+  },
+  {
+    "revision": "1d5451ced89eabb55683e27e070bdb60",
+    "url": "/frontend/static/media/studio-3.1d5451ce.jpg"
+  },
+  {
+    "revision": "e064d0908dbd53b55f8980c02b3748bb",
+    "url": "/frontend/static/media/studio-4.e064d090.jpg"
+  },
+  {
+    "revision": "ef5c30ea69b7ad740ee6221782c73741",
+    "url": "/frontend/static/media/studio-5.ef5c30ea.jpg"
+  }
+]);
